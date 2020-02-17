6c1243efa33e02a6937b7f05dcd0663eed287441
glmrvn <glmrvn@gmail.com>
2020-02-17 23:20:13 +0300

    Added colors counter

--- code.js
+++ code.js
@@ -1,12 +1,25 @@
+var day = "[day]"
+var night = "[night]"
+var allSelection
+
 // GET LIBRARY COLORS
 if (figma.command == 'get_colors') {
     async function setPaints() {
         await figma.clientStorage.setAsync('allColors', figma.getLocalPaintStyles().map(a => a.key))
     }
 
-    setPaints()
-    figma.closePlugin()
+    async function colorsNumber() {
+        var allColors = await figma.clientStorage.getAsync('allColors')
+        console.log(allColors.length)
 
+        if (allColors.length > 0){
+            figma.closePlugin(`👌 Saved colors: ${allColors.length}`)
+        } else { figma.closePlugin('😶 You dont have colors') }
+    }
+
+    setPaints()
+    colorsNumber()
+    // figma.closePlugin()
 }
 
 // SELECTED DARK MODE
@@ -25,13 +38,37 @@ if (figma.command == 'dark') {
             async function createRect() {
                 var id = await figma.importStyleByKeyAsync(paint_style).then(i => i.id)
 
-                const rect = figma.createRectangle();
-                rect.fillStyleId = id;
+                const rect = figma.createRectangle()
+                rect.fillStyleId = id
+                rect.remove()
             }
         })
     }
 
+// Changing colors
+    function findSelectedFrames() {
+        if (figma.currentPage.selection.length == 0) {
+            figma.closePlugin("🤔 No object selected.")
+        
+        } else if (!["FRAME", "COMPONENT", "INSTANCE"].includes(figma.currentPage.selection[0].type)) {
+            figma.closePlugin("👆🤓 Select frame or instance")
+        
+        } else {
+            const allSelection = figma.currentPage.selection[0].findAll()
+            allSelection.unshift(figma.currentPage.selection[0])
+        }
+
+        for (let index in allSelection) {
+            let frame = allSelection[index];
+
+            if (frame.fillStyleId == text_primary_day) {
+                frame.fillStyleId = text_primary_night
+            }
+        }
+    }
+
     getPaints()
+    findSelectedFrames()
     figma.closePlugin()
 
 }
